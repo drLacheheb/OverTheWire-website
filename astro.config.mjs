6589f1de4942ec8f251e9bd5527fc8eff0a295ee
drLacheheb <yacinolach@gmail.com>
2026-02-13 21:59:26 +0100

    chore: update base path in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ const isProd = process.env.NODE_ENV === 'production' || import.meta.env?.PROD;
 // https://astro.build/config
 export default defineConfig({
   site: 'https://drLacheheb.github.io',
-  base: isProd ? '/OverTheWire-website' : '/',
+  base: isProd ? '/OverTheWire-website/wargames/' : '/',
   vite: {
     plugins: [tailwindcss()],
   },
